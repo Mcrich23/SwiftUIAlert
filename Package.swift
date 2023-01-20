f727f576b2940852e8854ac4df93858c31d51465
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-01-20 14:24:52 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "SwiftUIAlert",
     platforms: [
         .iOS(.v14),
-        .watchOS(.v14),
+        .watchOS(.v7),
         .tvOS(.v14),
-        .macOS(.v14)
+        .macOS(.v11)
     ]
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
