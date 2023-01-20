fb99b46504951f24047cf66a73324e5ce9b8a9ab
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-01-20 14:24:24 -0800

    Added platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIAlert",
+    platforms: [
+        .iOS(.v14),
+        .watchOS(.v14),
+        .tvOS(.v14),
+        .macOS(.v14)
+    ]
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
